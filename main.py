a14a5e35405ebd9601597cfef3280c49c7f42467
Greegmon <149600509+Greegmon@users.noreply.github.com>
2024-06-25 23:55:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ app = ["Second", "Third", "Fourth"]
 with open('test.json', 'r') as fl:
 	data = json.load(fl)
 
-for _ in app:
-	data['msg'].append(_)
+oi = 0
+u = False
+if not u:
+	data['msg'].append(app)
+	u = True
+	oi += 1
 
 with open('test.json', 'w') as op:
 	json.dump(data, op, indent=2)
 
+st.write(oi)
+st.write(u)
 st.code(data)
