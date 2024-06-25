c54675cde013572dc723325f6089fea2d9c73959
Greegmon <149600509+Greegmon@users.noreply.github.com>
2024-06-25 23:38:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 import streamlit as st
-import requests
+import json
 
-st.header(":blue[Facebook React]", divider='rainbow')
+app = ["Second", "Third", "Fourth"]
+with open('test.json', 'r') as fl:
+	data = json.load(fl)
 
-cookie = st.text_area("Cookie")
-post = st.text_input("Link")
-x = ["👍 LIKE","❤️ LOVE","🤗 CARE","😆 HAHA","😲 WOW","😢 SAD","😡 ANGRY"]
-react = st.selectbox("Reaction", x)
+for _ in app:
+	data['msg'].append(_)
 
-def sendReact(cookie, post, react):
-	link = "https://flikers.net/android/android_get_react.php"
-	pay = {"post_id": post, "react_type": react, "version": 'v1.7'}
-	head = {
-		'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 12; V2134 Build/SP1A.210812.003)",
-		'Connection': "Keep-Alive",
-		'Accept-Encoding': "gzip",
-		'Content-Type': "application/json",
-		'Cookie': cookie
-  }
-	res = requests.post(link, json=pay, headers=head)
-	return True
+with open('test.json', 'w') as op:
+	json.dump(data, op, indent=2)
 
-btn = st.button("Submit", type='primary')
-if btn:
-	if not cookie or not post or not react:
-		st.error("Error, Missing input value")
-	else:
-		if "c_user=" in cookie:
-			st.success("Sending reacts...")
-			sendReact(cookie, post, react.split(' ')[1])
-		else:
-			st.error("Invalid Cookie")
+st.code(data)
